80e396b037e428212c1f5a6c7774792184265866
624-urvashi <143706170+624-urvashi@users.noreply.github.com>
2025-08-29 21:55:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const makeChange = (c) => {
   // your name here
+	et quarters = Math.floor(c/25);
+  c = c %25;
+  let dimes =Math.floor(c/10);
+    c = c %10;
+    let nickels = Math.floor(c/5);
+    c = c %5;
+    let pennies = c;  
+    return {q: quarters, d: dimes, n: nickels, p: pennies};
+
 };
 
 // Do not the change the code below
